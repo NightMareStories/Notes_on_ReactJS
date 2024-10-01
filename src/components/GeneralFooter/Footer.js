0ed38d6b7e8f0341c0308c6016c05c8d64a62812
Poteryaev Aleksandr <sp202037@yandex.ru>
2024-10-01 17:54:23 +0300

    Updated to v.1.1.1
    Links to external resources have been changed.

--- src/components/GeneralFooter/Footer.js
+++ src/components/GeneralFooter/Footer.js
@@ -16,7 +16,7 @@ function Footer(props) {
                     <nav className="g-block-footer__nav">
                         <ul className="g-block-footer__actions g-actions-footer">
                             <li><a href='https://github.com/NightMareStories/Notes_on_ReactJS.git' className="g-actions-footer__link" target="_blank" rel="noopener noreferrer"><span>GitHub</span></a></li>
-                            <li><a href='http://about-my-portfolio.site' className="g-actions-footer__link" target="_blank" rel="noopener noreferrer"><span>My Portfolio</span></a></li>
+                            <li><a href='http://about-my-portfolio.website' className="g-actions-footer__link" target="_blank" rel="noopener noreferrer"><span>My Portfolio</span></a></li>
                         </ul>
                     </nav>
                 </div>
